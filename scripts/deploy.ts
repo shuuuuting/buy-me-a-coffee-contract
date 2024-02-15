9645e8cb9daa7151f706ddebfa884b32cb9cab15
shuting <shutttingc@gmail.com>
2024-02-15 15:19:22 -0700

    feat: add script to deploy contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,19 @@
-import { ethers } from "hardhat";
+import { ethers } from "hardhat"
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const BuyMeATea = await ethers.getContractFactory("BuyMeATea")
+  const buyMeATea = await BuyMeATea.deploy()
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  await buyMeATea.waitForDeployment()
+  const buyMeATeaAddress = await buyMeATea.getAddress()
+  console.log("BuyMeATea deployed to:", buyMeATeaAddress)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
